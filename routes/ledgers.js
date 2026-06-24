f27b9487e839959aafc62cb0372b90e8e0f14b7c
rvincoy <vin23002@byui.edu>
2026-06-23 21:34:17 -0500

    update spelling

--- routes/ledgers.js
+++ routes/ledgers.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const ledgersController = require('../controllers/ledgers');
 
 router.get('/', ledgersController.getAll);
+  /* #swagger.tags = ['Ledgers'] */
+  /* #swagger.summary = 'Get all ledgers' */
+
+  /* #swagger.responses[200] = {
+        description: 'List of ledgers',
+        schema: [{
+          $ref: '#/components/schemas/Ledgers'
+        }]
+  } */
+
 router.get('/:id', ledgersController.getSingle);
 router.post('/', ledgersController.createLedger);
 router.put('/:id', ledgersController.updateLedger);
